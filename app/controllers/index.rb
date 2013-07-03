902152f21a954bd68468d686b0877664a6fbeee8
Navid Mostafavi <navidm@gmail.com>
2013-07-02 19:38:24 -0700

    finished

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,11 @@ end
 post '/color' do
 
   #Create and return a JSON object with the random cell and color given below.
-
-  cell= rand(1..9)
-  color= "#" + "%06x" % (rand * 0xffffff)
+  if request.xhr?
+    content_type :json
+    cell = rand(1..9)
+    color = "#" + "%06x" % (rand * 0xffffff)
+    {cell:  cell, color:  color}.to_json
+  end
+  
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
 
+  function setCellColor(cell, color) {
+    debugger
+    var current_cell = $('#color_me li').eq(cell-1);
+    current_cell.css('background-color', color);
+  }
+
+  $('#get_color').on('click', function (e) {
+    e.preventDefault();
+    var get_ajax = $.ajax({
+        url: '/color',
+        method: 'post',
+        dataType: 'json'
+      });
+
+    get_ajax.done(function (response) {
+      console.log(response);
+
+      setCellColor(response.cell, response.color);
+      // debugger; 
+    });
+
+  });
+
+
 });
